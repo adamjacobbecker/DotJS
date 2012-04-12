4600c21799786052cee4019eb91599e256c42eca
adamjacobbecker <ad@mbecker.cc>
2012-04-11 21:22:35 -0700

    first commit

---
+++ govhub.dev.js
@@ -0,0 +1,6 @@
+(function () {
+    var jsScript = document.createElement('script');
+    jsScript.setAttribute('type', 'text/javascript');
+    jsScript.text = "if(document.getElementById('sfWebDebug')){sfWebDebugToggleMenu()}";
+    document.getElementsByTagName('head')[0].appendChild(jsScript);
+})();
